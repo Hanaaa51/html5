9f47394f95c18c7421ed26e7a30631d6c47d4ba6
Hanaaa51 <ohhana4526@gmail.com>
2025-08-07 16:27:04 +0900

    유머튜브

---
+++ humortube/src/App.jsx
@@ -0,0 +1,51 @@
+function App() {
+ 
+  return (
+    <>
+    <div className="center_div">
+     <h1>웃긴 유튜브 영상 모음</h1>
+
+     <h2>☆FUN★</h2>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/2irpyc2FcAM?si=6z2ywSbenDX_J4ml" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/lDUpc2TWbtw?si=_DgO2RQM4uWFwOkd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/1TdotRJBz_U?si=3P_nPSFN67Sm7Xku" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/edyMqirNga8?si=flPs53VO-1SN5e0C" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/LOuy_4lrauM?si=dxtIHzhjmIe6HIBh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/Rufc3BIEf6w?si=XnLIZFLybSZXvP28" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/9WCxQtu8IA8?si=u1KY7jdrQrJ4ANbl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <h2>♬MUSIC♬</h2>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/lsx0NpK5LhU?si=RnWc3J_ajzsAzzpJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/34ZaddbF3jg?si=ai71kXpgBgzJSKRS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/8nq32TQBwXI?si=8MlZOgEGfdIR-2BS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/pcf9aH7WhN0?si=wMfA1DOl7vOpk5A9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/B_gKTJTIcYw?si=_HZfVHdDFv58zsu5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/J0_7zeFQS9M?si=3nE6A3VpLSR914t3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/Js67kofnQw0?si=pY7FxjuBWwQZiww0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/gVacR4GRZpw?si=Xn46tnRqPQc7YZ2Z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/w4T-FWyELng?si=7VpV6TP2U1rykucu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/O7n7DXU56Dw?si=QHoWYyNZ5Hx5SIEd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/u8mH-WHQdb0?si=GK6TyUvO1PCGk1_8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    </>
+  )
+}
+
+export default App
